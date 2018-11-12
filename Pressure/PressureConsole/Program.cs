c09460a4ca9b7a3371d40343da28e4a8edf1fa39
Chris <31527886+TheLeeanderthal@users.noreply.github.com>
2018-11-12 14:03:09 -0500

    Adding Console UI
    Created Console Project

---
+++ Pressure/PressureConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.IO;
+
+using Engine;
+
+namespace PressureConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
